755d839beeef393dc24daf2bdc3d1888b9541a69
gamoyo525 <m1o6m6o@gmail.com>
2025-04-03 22:20:02 +0900

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from the proxy server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
