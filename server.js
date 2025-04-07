07272b50def5a8120ff3604e0ad0c2abbffeb207
gamoyo525 <m1o6m6o@gmail.com>
2025-04-07 23:08:40 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,13 @@ const app = express();
 app.use(cors());
 
 const BASE_URL = "https://newsapi.org";
+const API_KEY = "1a5a88eb386244ff9e7f82966e28e009"; // 
 
 app.use("/", async (req, res) => {
-  const url = `${BASE_URL}${req.url}`;
-  const apiKey = req.headers["x-api-key"]; // ← フロントエンドからこのヘッダーで送る！
-
-  if (!apiKey) {
-    return res.status(400).json({ error: "Missing API key in request headers" });
-  }
+  const url = `${BASE_URL}${req.url}${req.url.includes("?") ? "&" : "?"}apiKey=${API_KEY}`;
 
   try {
-    const response = await fetch(url + `&apiKey=${apiKey}`);
+    const response = await fetch(url);
     const data = await response.json();
     res.status(response.status).json(data);
   } catch (err) {
